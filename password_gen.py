f04a846670ee2758a8f027fa5f390dff36696e27
ntritran999 <81624097+ntritran999@users.noreply.github.com>
2021-05-20 20:44:32 +0700

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -4,7 +4,7 @@ def password_generator():
     charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345679!@#$%^&*()"
     password = []
 
-    for i in range(16): # You can use whatever range you like.
+    for _ in range(16): # You can use whatever range you like.
         rand = random.choice(charset)
         password.append(rand)
     return "".join(password)
